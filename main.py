e905ea6556c9aa86ee78beabcdbfabc439a50efd
Num Gamer <>
2021-05-19 13:21:51 +0000

    small change

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord,jsonh
+import discord,jsonh,os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = "un ")
@@ -15,4 +15,4 @@ async def on_command_error(ctx,error):
 client.load_extension("disgame")
 client.load_extension("admin")
 
-client.run("ODQzNzMzNzE2MTk0OTUxMTk5.YKIKQQ.se80T5iK-5segnctRQKQEyMN-bE")
+client.run(os.environ["token"])
